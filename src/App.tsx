a4e7e8649126e57f78b2b1989606af8fa20d28dd
Toxyk <toxyflog.games@gmail.com>
2022-04-02 09:17:00 +0300

    Apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,6 @@ import styled from "styled-components";
 
 const Style = styled.p`
   font-size: 24px;
-  color: black;
-  background: rebeccapurple;
-  width: 200px;
-  height: 200px;
-  display: block;
-  font-family: sans-serif;
-  font-weight: 900;
-  text-decoration: underline;
-  text-shadow: 5px 5px 5px blue;
 `;
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import App from "./App";
 import GlobalStyles from "./styles";
 import * as ReactDOMClient from "react-dom/client";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
 const loading: Element | null = document.getElementById("loading");
 if (loading) loading.remove();
 
+
+const client = new ApolloClient({
+	uri: "http://localhost:3001",
+	cache: new InMemoryCache(),
+});
+
 const root: Element | null = document.getElementById("root");
 if (!root) throw new Error("Couldn't mount app - no element matched the selector!");
-
 const app = ReactDOMClient.createRoot(root);
+
 app.render(
-	<>
+	<ApolloProvider client={client}>
 		<GlobalStyles/>
 		<App/>
-	</>,
+	</ApolloProvider>,
 );
